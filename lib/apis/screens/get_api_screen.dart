cd065005e93ea93bafbf2758926539c8385f1418
Usman-bin-Khalid <usmanbinkhalidpk@gmail.com>
2026-07-28 12:56:43 +0500

    Get Post API Completed

--- lib/apis/screens/get_api_screen.dart
+++ lib/apis/screens/get_api_screen.dart
@@ -1,1 +1,61 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
+import 'package:riverpod_code/apis/models/post_model.dart';
+
+class GetApiScreen extends StatefulWidget {
+  const GetApiScreen({super.key});
+
+  @override
+  State<GetApiScreen> createState() => _GetApiScreenState();
+}
+
+class _GetApiScreenState extends State<GetApiScreen> {
+  List<PostModel> postList = [];
+  Future<List<PostModel>> getPostAPI() async {
+    final response = await http.get(
+      Uri.parse('https://jsonplaceholder.typicode.com/posts'),
+    );
+    var data = jsonDecode(response.body.toString());
+    if (response.statusCode == 200) {
+      for (Map<String, dynamic> i in data) {
+        postList.add(PostModel.fromJson(i));
+      }
+      return postList;
+    } else {
+      return postList;
+    }
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('API Course')),
+      body: Column(
+        children: [
+          Expanded(
+            child: FutureBuilder(
+              future: getPostAPI(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return CircularProgressIndicator();
+                } else {
+                  return ListView.builder(
+                    itemCount: postList.length,
+                    itemBuilder: (context, index) {
+                      return Text(postList[index].body.toString());
+                    },
+                  );
+                }
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
